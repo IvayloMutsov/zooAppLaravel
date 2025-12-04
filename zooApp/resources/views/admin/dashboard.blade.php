467be884eb8755b6bfcb0e5ab84478f8aa82849d
IvayloMutsov <145490251+IvayloMutsov@users.noreply.github.com>
2025-12-04 23:27:10 +0000

    error fix 1

---
+++ zooApp/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layout')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<div class="container">
+    <h1>Добре дошли в админ панела</h1>
+    <p>Тук може да управлявате животни, породи, видове и потребители.</p>
+</div>
+@endsection

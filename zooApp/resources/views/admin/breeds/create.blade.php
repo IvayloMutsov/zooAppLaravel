bee1af170128f0905a6500fec068c55f329880a9
IvayloMutsov <145490251+IvayloMutsov@users.noreply.github.com>
2025-12-05 18:08:19 +0000

    error fix 10

--- zooApp/resources/views/admin/breeds/create.blade.php
+++ zooApp/resources/views/admin/breeds/create.blade.php
@@ -1,7 +1,13 @@
-<select name="type_id" class="form-control">
-    @foreach($types as $type)
-        <option value="{{ $type->id }}" {{ (old('type_id', $breed->type_id ?? '') == $type->id) ? 'selected' : '' }}>
-            {{ $type->name }}
-        </option>
-    @endforeach
-</select>
+form action="{{ route('admin.breeds.store') }}" method="POST">
+    @csrf
+
+    <select name="type_id" class="form-control">
+        @foreach($types as $type)
+            <option value="{{ $type->id }}" {{ old('type_id', $breed->type_id ?? '') == $type->id ? 'selected' : '' }}>
+                {{ $type->name }}
+            </option>
+        @endforeach
+    </select>
+
+    <button type="submit" class="btn btn-primary mt-2">Save Breed</button>
+</form>
